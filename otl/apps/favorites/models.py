6cb0543e217baf30e80a999f55a08894ef29eb14
daybreaker <devnull@localhost>
2009-04-03 23:18:54 +0900

    과목 즐겨찾기 model 수정

--- otl/apps/favorites/models.py
+++ otl/apps/favorites/models.py
@@ -1,18 +1,14 @@
+# encoding: utf8
 from django.db import models
 from django.contrib.auth.models import User
 
 class CourseLink(models.Model):
-	lec_code = models.CharField(max_length=10)
+	course_code = models.CharField(max_length=10)
 	year = models.IntegerField()
-	semester = models.CharField(max_length=1)
-	url = models.URLField(max_length=200)
-	cnt = models.IntegerField()
-	writer_id = models.IntegerField()
-	wirte_time = models.DateTimeField()
-	favorite_list = models.ManyToManyField(User, through = 'FavoriteList')
-
-class FavoriteList(models.Model):
-	user = models.ForeignKey(User)
-	link = models.ForeignKey(CourseLink)
-	
+	semester = models.SmallIntegerField()
+	url = models.URLField()
+	favored_count = models.IntegerField()
+	writer = models.ForeignKey(User, related_name='courselink_set')
+	written = models.DateTimeField() # written, last_updated는 관습적으로 작성/수정 시간을 표현하는 데 사용됨
+	favored_by = models.ManyToManyField(User, related_name='favorite_set')
 
