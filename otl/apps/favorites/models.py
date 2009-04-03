3b906f9a58a9e51d710a4507f82f2fc814c8cb70
daybreaker <devnull@localhost>
2009-04-03 23:54:16 +0900

    과목 즐겨찾기 보기 기능 1차 구현

--- otl/apps/favorites/models.py
+++ otl/apps/favorites/models.py
@@ -1,9 +1,11 @@
 # encoding: utf8
 from django.db import models
+from django.contrib import admin
 from django.contrib.auth.models import User
 
 class CourseLink(models.Model):
 	course_code = models.CharField(max_length=10)
+	course_name = models.CharField(max_length=60)
 	year = models.IntegerField()
 	semester = models.SmallIntegerField()
 	url = models.URLField()
@@ -12,3 +14,8 @@ class CourseLink(models.Model):
 	written = models.DateTimeField() # written, last_updated는 관습적으로 작성/수정 시간을 표현하는 데 사용됨
 	favored_by = models.ManyToManyField(User, related_name='favorite_set')
 
+class CourseLinkAdmin(admin.ModelAdmin):
+	list_display = ('course_code', 'course_name', 'year', 'semester', 'url', 'writer')
+	ordering = ('-written',)
+
+admin.site.register(CourseLink, CourseLinkAdmin)
--- otl/apps/favorites/views.py
+++ otl/apps/favorites/views.py
@@ -1,8 +1,34 @@
 # encoding: utf-8
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.paginator import Paginator
+from django.contrib.auth.models import User
+from otl.apps.favorites.models import CourseLink
+
+SEMESTER_NAMES = {
+	1: u'봄',
+	2: u'여름',
+	3: u'가을',
+	4: u'겨울',
+}
+NUM_PER_PAGE = 10
 
 def index(request):
+	if request.user.is_authenticated():
+		favorite_list = CourseLink.objects.filter(favored_by__exact=request.user)
+	else:
+		favorite_list = None
+	
+	page = request.GET.get('page', 1)
+	courselink_pages = Paginator(CourseLink.objects.all().order_by('-written'), NUM_PER_PAGE)
+	current_page = courselink_pages.page(page)
+	# TODO: 나중에 영문 과목명과 한글 과목명 처리는 어떻게?
+
 	return render_to_response('favorites/index.html', {
 		'section': 'favorites',
+		'current_year': 2009, # TODO: 공통적으로 사용할 수 있게 middleware로 처리하는 게 좋을 듯.
+		'current_semester': SEMESTER_NAMES[1],
+		'favorite_list': favorite_list,
+		'recently_added_list': current_page.object_list,
+		'current_page': current_page,
 	}, context_instance=RequestContext(request))
