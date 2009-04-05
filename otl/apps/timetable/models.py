7e3e3d424931551c684c97096c2e11f8c9841b20
daybreaker <devnull@localhost>
2009-04-05 18:02:52 +0900

    Django admin에서 lecture 관련 정보 다룰 수 있게 하고 빠진 학년구분 필드 하나 추가

--- otl/apps/timetable/models.py
+++ otl/apps/timetable/models.py
@@ -1,23 +1,45 @@
 # encoding: utf-8
 from django.db import models
+from django.contrib import admin
 from otl.apps.accounts.models import Department
 
 CLASS_TYPES = (
 	('l', u'강의'),
 	('e', u'실험'),
 )
+SEMESTER_TYPES = (
+	(1, '봄'),
+	(2, '여름'),
+	(3, '가을'),
+	(4, '겨울'),
+)
+AUDIENCE_TYPES = (
+	(0, u'학사'),
+	(7, u'상호인정'),
+	(8, u'대학원'),
+)
+WEEKDAYS = (
+	(1, u'월'),
+	(2, u'화'),
+	(3, u'수'),
+	(4, u'목'),
+	(5, u'금'),
+	(6, u'토'),
+	(7, u'일'),
+)
 
 class Lecture(models.Model):
 	"""특정 년도·학기에 개설된 과목 instance를 가리키는 모델"""
 	code = models.CharField(max_length=10)					# 과목코드
 	year = models.IntegerField()							# 개설년도 (4자리 숫자)
-	semester = models.SmallIntegerField()		   			# 개설학기 (1=봄, 2=여름, 3=가을, 4=겨울)
+	semester = models.SmallIntegerField(choices=SEMESTER_TYPES)	# 개설학기 (1=봄, 2=여름, 3=가을, 4=겨울)
 	department = models.ForeignKey(Department)	   			# 학과
 	class_no = models.CharField(max_length=4)				# 분반
 	title = models.CharField(max_length=100)	   			# 과목이름 (한글)
 	title_en = models.CharField(max_length=100)	   			# 과목이름 (영문)
 	type = models.CharField(max_length=12)		   			# 과목구분 (한글; '전필', '전선', '기필', ...)
 	type_en = models.CharField(max_length=36)	   			# 과목구분 (영문; 'Major Required', 'Major Elective', ...)
+	audience = models.IntegerField(choices=AUDIENCE_TYPES)	# 학년구분
 	credit = models.IntegerField()				   			# 학점
 	credit_au = models.IntegerField()			   			# AU
 	class_time = models.CharField(max_length=100, null=True)	# 수업시간 (문자열로 설명만 되어 있는 것)
@@ -28,15 +50,29 @@ class Lecture(models.Model):
 	notice = models.CharField(max_length=200, null=True)	# 비고
 	is_english = models.BooleanField()						# 영어강의 여부
 
+	def __unicode__(self):
+		return '%s (%d:%s) %s' % (self.code, self.year, self.get_semester_display(), self.title)
+
 	class Meta:
 		unique_together = ('code', 'year', 'semester', 'department', 'class_no')
+
+class LectureAdmin(admin.ModelAdmin):
+	list_display = ('code', 'year', 'semester', 'department', 'class_no', 'title', 'professor', 'type', 'audience', 'credit', 'credit_au', 'limit')
+	ordering = ('-year', '-semester', 'code')
 
 class ExamTime(models.Model):
 	"""Lecture에 배정된 시험 시간."""
 	lecture = models.ForeignKey(Lecture)
-	day = models.SmallIntegerField()				# 시험 요일 (1~5: 월~금)
-	begin = models.CharField(max_length=5)			# hh:mm 형태의 시험 시작 시간 (24시간제)
-	end = models.CharField(max_length=5)			# hh:mm 형태의 시험 종료 시간 (24시간제)
+	day = models.SmallIntegerField(choices=WEEKDAYS)	# 시험 요일
+	begin = models.CharField(max_length=5)				# hh:mm 형태의 시험 시작 시간 (24시간제)
+	end = models.CharField(max_length=5)				# hh:mm 형태의 시험 종료 시간 (24시간제)
+
+	def __unicode__(self):
+		return '[%s] %s, %s-%s' % (self.lecture.code, self.get_day_display(), self.begin, self.end)
+
+class ExamTimeAdmin(admin.ModelAdmin):
+	list_display = ('lecture', 'day', 'begin', 'end')
+	ordering = ('lecture',)
 
 class ClassTime(models.Model):
 	"""Lecture에 배정된 강의 시간. 보통 하나의 Lecture가 여러 개의 강의 시간을 가진다."""
@@ -51,6 +87,13 @@ class ClassTime(models.Model):
 	room_en = models.CharField(max_length=60)	# 수업 장소 (영문)
 	unit_time = models.SmallIntegerField()		# 수업 교시
 
+	def __unicode__(self):
+		return '[%s] %s, %s-%s @%s' % (self.lecture.code, self.get_day_display(), self.begin, self.end, self.room_ko)
+
+class ClassTimeAdmin(admin.ModelAdmin):
+	list_display = ('lecture', 'day', 'begin', 'end', 'room_ko')
+	ordering = ('lecture',)
+
 class Syllabus(models.Model):
 	"""Lecture에 대한 강의계획서 정보. 첨부파일로만 나오고 비어있는 경우도 있음."""
 	lecture = models.ForeignKey(Lecture)
@@ -63,6 +106,17 @@ class Syllabus(models.Model):
 	url = models.URLField(null=True)							# 과목 홈페이지
 	attachment = models.CharField(max_length=260, null=True)	# 첨부파일 이름
 
+	def __unicode__(self):
+		return '%s (%s)' % (self.lecture.code, self.professor_info)
+
+class SyllabusAdmin(admin.ModelAdmin):
+	list_display = ('lecture', 'professor_info', 'abstract', 'url', 'attachment')
+	ordering = ('lecture',)
+
 # TODO: 수강신청 현황 View에 대응하는 Table
 
+admin.site.register(Lecture, LectureAdmin)
+admin.site.register(ExamTime, ExamTimeAdmin)
+admin.site.register(ClassTime, ClassTimeAdmin)
+admin.site.register(Syllabus, SyllabusAdmin)
 # vim: set ts=4 sts=4 sw=4 noet:
