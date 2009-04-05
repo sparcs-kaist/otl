0e2fd0b44a08cad9f7286ab16d212a01d8b705f4
majja <devnull@localhost>
2009-04-05 23:22:29 +0900

    에디터 설정 문제로 cp949로 잘못 저장된 것을 utf-8로 다시 저장

--- otl/apps/favorites/views.py
+++ otl/apps/favorites/views.py
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# encoding: utf8
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.paginator import Paginator
@@ -7,10 +7,10 @@ from otl.apps.favorites.models import CourseLink
 import time
 
 SEMESTER_NAMES = {
-	1: u'��',
-	2: u'����',
-	3: u'����',
-	4: u'�ܿ�',
+	1: u'봄',
+	2: u'여름',
+	3: u'가을',
+	4: u'겨울',
 }
 NUM_PER_PAGE = 10
 
@@ -23,11 +23,11 @@ def index(request):
 	page = request.GET.get('page', 1)
 	courselink_pages = Paginator(CourseLink.objects.all().order_by('-written'), NUM_PER_PAGE)
 	current_page = courselink_pages.page(page)
-	# TODO: ?�중???�문 과목명과 ?��? 과목�?처리???�떻�?
+	# TODO: 나중에 영문 과목명과 한글 과목명 처리는 어떻게?
 
 	return render_to_response('favorites/index.html', {
 		'section': 'favorites',
-		'current_year': 2009, # TODO: 공통?�으�??�용?????�게 middleware�?처리?�는 �?좋을 ??
+		'current_year': 2009, # TODO: 공통적으로 사용할 수 있게 middleware로 처리하는 게 좋을 듯.
 		'current_semester': SEMESTER_NAMES[1],
 		'favorite_list': favorite_list,
 		'recently_added_list': current_page.object_list,
@@ -62,7 +62,7 @@ def add(request):
 
 	return render_to_response('favorites/index.html', {
 		'section': 'favorites',
-		'current_year': 2009, # TODO: 공통?�으�??�용?????�게 middleware�?처리?�는 �?좋을 ??
+		'current_year': 2009,
 		'current_semester': SEMESTER_NAMES[1],
 		'favorite_list': favorite_list,
 		'recently_added_list': current_page.object_list,
@@ -88,9 +88,6 @@ def create(request):
 		'recently_added_list': current_page.object_list,
 		'current_page': current_page,
 	}, context_instance=RequestContext(request))
-
-# def add(request):
-
 
 def delete(request):
 	course_id = request.GET.get('delete')
