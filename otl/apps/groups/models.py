ccf17cab622dfee6b86d20f14421a58d0809874a
majja <devnull@localhost>
2009-04-19 22:48:01 +0900

    refs #9: 조모임보드 모델 추가

---
+++ otl/apps/groups/models.py
@@ -0,0 +1,29 @@
+# encoding: utf8
+from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import User
+from otl.apps.common import *
+
+class GroupBoard(models.Model):
+	course_code = models.CharField(max_length=10)
+	course_name = models.CharField(max_length=60)
+	group_name = models.CharField(max_length=60)
+	comment = models.CharField(max_length=100)
+	passwd = models.CharField(max_length=10)
+	year = models.IntegerField()
+	semester = models.SmallIntegerField(choices=SEMESTER_TYPES)
+	maker = models.ForeignKey(User, related_name='groupboard_set')
+	made = models.DateTimeField()
+	group_in = models.ManyToManyField(User, related_name='group_set')
+
+class GroupArticle(models.Model):
+	group = models.ForeignKey(GroupBoard, related_name='article_set')
+	tag = models.CharField(max_length=500)
+	writer = models.ForeignKey(User, related_name='writer_set')
+	written = models.DateTimeField()
+
+class GroupBoardAdmin(admin.ModelAdmin):
+	list_display = ('course_code', 'course_name', 'group_name', 'year', 'semester', 'maker')
+	ordering = ('-made',)
+
+admin.site.register(GroupBoard, GroupBoardAdmin)
--- otl/apps/groups/views.py
+++ otl/apps/groups/views.py
@@ -31,14 +31,16 @@ def index(request):
 
 def create(request):
 	if request.user.is_authenticated():
-		new_code = request.GET.get('code')
-		new_course_name = request.GET.get('cname')
-		new_group_name = request.GET.get('gname')
-		new_year = request.GET.get('year')
-		new_semester = request.GET.get('semester')
+		new_code = request.POST.get('code')
+		new_course_name = request.POST.get('cname')
+		new_group_name = request.POST.get('gname')
+		new_pw = request.POST.get('passwd')
+		new_comment = request.POST.get('comment')
+		new_year = request.POST.get('year')
+		new_semester = request.POST.get('semester')
 		new_maker = request.user
 		new_made = time.strftime('%Y-%m-%d %H:%M:%S')
-		new_group = GroupBoard.objects.create(course_code = new_code, course_name = new_course_name, group_name = new_group_name, year = new_year, semester = new_semester, maker = new_maker, made = new_made)
+		new_group = GroupBoard.objects.create(course_code = new_code, course_name = new_course_name, group_name = new_group_name, password = new_pw, comment = new_comment, year = new_year, semester = new_semester, maker = new_maker, made = new_made)
 		new_group.group_in.add(new_maker);
 	else:
 		group_list=None
