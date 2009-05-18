a8b954a2e0f330f2df431e36092dccc519c9f867
daybreaker <devnull@localhost>
2009-05-18 17:18:54 +0900

    refs #35: mootools 및 common.js를 모든 페이지에 기본으로 포함시키고 Notifier 클래스를 좀더 추상화시킴.

--- otl/media/js/common.js
+++ otl/media/js/common.js
@@ -7,158 +7,75 @@
  */
 
 var Notifier = {
-	initialize: function()
+	/*
+	 * Requires at least 3 elements in this structure:
+	 * <div id="indicator"></div>
+	 * <div id="message-wrap">
+	 *   <div id="message"></div>
+	 * </div>
+	 */
+	initialize: function(options)
 	{
-		// Nothing to do.
+		var my_options = {
+			indicator: $('indicator'),
+			message: $('message'),
+			wrapper: $('message-wrap'),
+			clear_timeout: 10000,
+		};
+		$extend(my_options, options);
+		this.indicator = my_options.indicator;
+		this.message = my_options.message;
+		this.wrapper = my_options.wrapper;
+		this.clear_timeout = my_options.clear_timeout;
 	},
 	showIndicator: function()
 	{
-		$('indicator').addClass('waiting');
+		this.indicator.addClass('waiting');
 	},
 	setLoadingMsg: function(msg)
 	{
 		if (this.timeout)
 			window.clearTimeout(this.timeout);
-		$('indicator').addClass('waiting');
-		$('message').set('html', msg);
-		$('message-wrap').fade('show');
+		this.indicator.addClass('waiting');
+		this.message.set('html', msg);
+		this.wrapper.fade('show');
 	},
 	setMsg: function(msg)
 	{
 		this.clearIndicator();
-		$('message').set('html', msg);
-		$('message-wrap').fade('show');
-		$('message-wrap').highlight('#FAD163');
+		this.message.set('html', msg);
+		this.wrapper.fade('show');
+		this.wrapper.highlight('#FAD163');
 		if (this.timeout)
 			window.clearTimeout(this.timeout);
-		this.timeout = window.setTimeout(function() {
-			$('message-wrap').fade('out');
-		}, 10000);
+		if (this.clear_timeout > 0)
+			this.timeout = window.setTimeout(function() {
+				this.wrapper.fade('out');
+			}, this.clear_timeout);
 	},
 	setErrorMsg: function(msg)
 	{
 		this.clearIndicator();
-		$('message').set('html', msg);
-		$('message-wrap').fade('show');
-		$('message-wrap').highlight('#E55');
+		this.message.set('html', msg);
+		this.wrapper.fade('show');
+		this.wrapper.highlight('#E55');
 		if (this.timeout)
 			window.clearTimeout(this.timeout);
-		this.timeout = window.setTimeout(function() {
-			$('message-wrap').fade('out');
-		}, 10000);
+		if (this.clear_timeout > 0)
+			this.timeout = window.setTimeout(function() {
+				this.wrapper.fade('out');
+			}, this.clear_timeout);
 	},
 	clearIndicator: function()
 	{
-		$('indicator').removeClass('waiting');
+		this.indicator.removeClass('waiting');
 	},
 	clear: function()
 	{
 		if (this.timeout)
 			window.clearTimeout(this.timeout);
 		this.clearIndicator();
-		$('message-wrap').fade('hide');
-	}
-}
-
-var Map = {
-	initialize:function()
-	{
-		this.container = $('map-drag-container');
-		this.dragmap = $('dragmap');
-		this.maptext= $('map-text');
-		this.settings = 
-		{
-			cW:this.container.offsetWidth,
-			cH:this.container.offsetHeight,
-			dW:this.dragmap.offsetWidth,
-			dH:this.dragmap.offsetHeight
-		}
-
-		this.dragging = false;
-		this.clickPos = null
-
-		this.dragmap.setStyle('left',-258);
-		this.dragmap.setStyle('top',-270);
-		this.registHandles();
-	},
-	registHandles:function()
-	{
-		this.fx = new Fx.Morph(this.dragmap, {
-			wait:false,
-			duration:250,
-			transition: Fx.Transitions.Quad.easeInOut
-			});
-
-		this.dragHandler = this.onDrag.bindWithEvent(this);
-		this.dragEndHandler = this.onEnd.bindWithEvent(this);
-		this.dragmap.addEvent('mousedown',this.onMousedown.bindWithEvent(this));
-	},
-	onMousedown:function(e)
-	{
-		this.dragging=true;
-		document.addEvent('mousemove', this.dragHandler);
-		document.addEvent('mouseup', this.dragEndHandler);
-		this.clickPos = UDF.mousePos(e,this.dragmap);
-		e.stop();
-	},
-	onDrag:function(e)
-	{
-		if (this.dragging)
-		{
-			var pos = UDF.mousePos(e,this.container);
-			pos.x-=this.clickPos.x;
-			pos.y-=this.clickPos.y;
-
-			var s = this.settings;
-			var left = (pos.x).limit((s.cW-s.dW+2), 0);
-			var top = (pos.y).limit((s.cH-s.dH+2), 0);
-			this.dragmap.style.left=left+'px';
-			this.dragmap.style.top=top+'px';
-			this.previous_target = null;
-
-			e.stop();
-		}
-	},
-	onEnd:function(e)
-	{
-		document.removeEvent('mousemove', this.dragHandler);
-		document.removeEvent('mouseup', this.dragEndHandler);
-		if (this.dragging)
-		{
-			this.dragging=false;
-			e.stop();
-		}
-	},
-	move:function(x,y)
-	{
-		var s = this.settings;
-
-		x-=(s.cW/2);
-		y-=(s.cH/2);
-
-		var left = (x).limit(0, s.dW-s.cW);
-		var top = (y).limit(0, s.dH-s.cH);
-		this.fx.start({
-				left : (-left),
-				top :(-top)
-			});
-	},
-	find:function(name)
-	{
-		var arr = Data.Map.filter(function(item)
-		{
-			return item.name==name;
-		});
-		if (arr.length==0) return;
-
-		var item = arr[0];
-		var x = item.x;
-		var y = item.y;
-		this.maptext.style.left=(x-6)+'px';
-		this.maptext.style.top=(y-60)+'px';
-		$('map-name').innerHTML=item.code+' '+item.name;
-		if (this.previous_target != item.name)
-			this.move(x,y);
-		this.previous_target = item.name;
+		this.wrapper.fade('hide');
 	}
 };
+
--- otl/media/js/timetable.js
+++ otl/media/js/timetable.js
@@ -157,6 +157,109 @@ var UDF = {
 	}
 }
 
+var Map = {
+	initialize:function()
+	{
+		this.container = $('map-drag-container');
+		this.dragmap = $('dragmap');
+		this.maptext= $('map-text');
+		this.settings = 
+		{
+			cW:this.container.offsetWidth,
+			cH:this.container.offsetHeight,
+			dW:this.dragmap.offsetWidth,
+			dH:this.dragmap.offsetHeight
+		}
+
+		this.dragging = false;
+		this.clickPos = null
+
+		this.dragmap.setStyle('left',-258);
+		this.dragmap.setStyle('top',-270);
+		this.registHandles();
+	},
+	registHandles:function()
+	{
+		this.fx = new Fx.Morph(this.dragmap, {
+			wait:false,
+			duration:250,
+			transition: Fx.Transitions.Quad.easeInOut
+			});
+
+		this.dragHandler = this.onDrag.bindWithEvent(this);
+		this.dragEndHandler = this.onEnd.bindWithEvent(this);
+		this.dragmap.addEvent('mousedown',this.onMousedown.bindWithEvent(this));
+	},
+	onMousedown:function(e)
+	{
+		this.dragging=true;
+		document.addEvent('mousemove', this.dragHandler);
+		document.addEvent('mouseup', this.dragEndHandler);
+		this.clickPos = UDF.mousePos(e,this.dragmap);
+		e.stop();
+	},
+	onDrag:function(e)
+	{
+		if (this.dragging)
+		{
+			var pos = UDF.mousePos(e,this.container);
+			pos.x-=this.clickPos.x;
+			pos.y-=this.clickPos.y;
+
+			var s = this.settings;
+			var left = (pos.x).limit((s.cW-s.dW+2), 0);
+			var top = (pos.y).limit((s.cH-s.dH+2), 0);
+			this.dragmap.style.left=left+'px';
+			this.dragmap.style.top=top+'px';
+			this.previous_target = null;
+
+			e.stop();
+		}
+	},
+	onEnd:function(e)
+	{
+		document.removeEvent('mousemove', this.dragHandler);
+		document.removeEvent('mouseup', this.dragEndHandler);
+		if (this.dragging)
+		{
+			this.dragging=false;
+			e.stop();
+		}
+	},
+	move:function(x,y)
+	{
+		var s = this.settings;
+
+		x-=(s.cW/2);
+		y-=(s.cH/2);
+
+		var left = (x).limit(0, s.dW-s.cW);
+		var top = (y).limit(0, s.dH-s.cH);
+		this.fx.start({
+				left : (-left),
+				top :(-top)
+			});
+	},
+	find:function(name)
+	{
+		var arr = Data.Map.filter(function(item)
+		{
+			return item.name==name;
+		});
+		if (arr.length==0) return;
+
+		var item = arr[0];
+		var x = item.x;
+		var y = item.y;
+		this.maptext.style.left=(x-6)+'px';
+		this.maptext.style.top=(y-60)+'px';
+		$('map-name').innerHTML=item.code+' '+item.name;
+		if (this.previous_target != item.name)
+			this.move(x,y);
+		this.previous_target = item.name;
+	}
+};
+
 var LectureList = {
 	initialize:function(tabs,contents)
 	{
