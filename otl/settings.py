b13a0651bf29ba7f995b25450132321614197ae9
daybreaker <devnull@localhost>
2009-05-22 02:25:28 +0900

    AuthenticationMiddleware를 CachedAuthMiddleware로 교체하여 로그인 상태의 http request마다 user 정보를 DB에서 읽어올 필요 없게 함.

--- otl/settings.py
+++ otl/settings.py
@@ -71,7 +71,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'otl.utils.middleware.CachedAuthMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
 ROOT_URLCONF = 'otl.urls'
