ebcae1cf4a68a95ac9741db7c6eae80e522f99ef
daybreaker <devnull@localhost>
2009-05-24 19:23:08 +0900

    refs #10: Sybase, MySQLdb 안 깔려 있어도 일단 동작하게 고치고 추가 작업

--- otl/apps/calendar/models.py
+++ otl/apps/calendar/models.py
@@ -7,7 +7,11 @@ from otl.apps.accounts.models import Department
 from otl.utils import MultiSelectField, get_choice_display
 from otl.apps.common import *
 from datetime import datetime
-import Sybase, MySQLdb
+try:
+	import Sybase
+	import MySQLdb
+except ImportError:
+	pass
 
 class Calendar(models.Model):
 	owner = models.ForeignKey(User)
@@ -22,21 +26,34 @@ class CalendarAdmin(admin.ModelAdmin):
 	ordering = ('owner', 'title')
 
 class RepeatedSchedule(models.Model):
+	# TODO: 전체 일정 개수를 셀 때, repeated schedule + schedule NOT a part of repeated schedule로 계산
 	belongs_to = models.ForeignKey(Calendar)
 	rule = models.IntegerField(choices=SCHEDULE_REPEAT_TYPES)
+	summary = models.CharField(max_length=120)
+	location = models.CharField(max_length=120, blank=True)
 	weekdays = MultiSelectField(choices=WEEKDAYS, max_length=20, blank=True)
 	day_of_months = MultiSelectField(choices=MONTHDAYS, max_length=80, blank=True)
+	# TODO: lazy-creation of schedule items
+	#       처음 반복일정을 생성할 때 그에 해당하는 Schedule 오브젝트를 한번에
+	#       생성하지 않고, 해당 오브젝트가 들어있어야 하는 날짜 범위에 대한 요청이
+	#       들어온 경우에만 생성한다. (1달치 정도는 그냥 해도 될 듯.)
 	date_begin = models.DateField(null=True, blank=True)
 	date_end = models.DateField(null=True, blank=True)
 	time_begin = models.TimeField(null=True, blank=True)
 	time_end = models.TimeField(null=True, blank=True)
+	description = models.TextField(blank=True)
 
 	def __unicode__(self):
+		rule = u''
 		if self.rule == 1: # 매주 일정
+			rule = u'매주'
 			rules = u', '.join(map(lambda day: get_choice_display(WEEKDAYS, int(day)), self.weekdays))
 		elif self.rule == 2: # 매월 일정
+			rule = u'매월'
+			rules = u', '.join(map(lambda day: get_choice_display(WEEKDAYS, int(day)), self.weekdays))
 			rules = u'일, '.join(self.day_of_months) + u'일'
 		else:
+			rule = u'?'
 			rules = u'(unrecognized repetition rule)'
 		try:
 			date_range = u'%s - %s' % (self.date_begin.strftime('%m/%d'), self.date_end.strftime('%m/%d'))
@@ -46,7 +63,7 @@ class RepeatedSchedule(models.Model):
 			time_range = u'%s - %s' % (self.time_begin.strftime('%H:%M'), self.time_end.strftime('%H:%M'))
 		except:
 			time_range = u'종일'
-		return u'%s 반복(%s): %s @ %s' % (self.get_rule_display(), date_range, time_range, rules)
+		return u'%s 반복(%s): %s @ %s' % (rule, date_range, time_range, rules)
 
 class RepeatedScheduleAdmin(admin.ModelAdmin):
 	list_display = ('belongs_to', '__unicode__')
@@ -56,14 +73,25 @@ class Schedule(models.Model):
 	belongs_to = models.ForeignKey(Calendar)
 	one_of = models.ForeignKey(RepeatedSchedule, null=True, blank=True)
 	summary = models.CharField(max_length=120)
-	location = models.CharField(max_length=120)
+	location = models.CharField(max_length=120, blank=True)
 	date = models.DateField()
 	begin = models.TimeField()
 	end = models.TimeField()
 	description = models.TextField(blank=True)
 
+	def separate_from_repeated(self):
+		self.summary = self.one_of.summary
+		self.description = self.one_of.description
+		self.location = self.one_of.location
+		self.one_of = None
+		self.save()
+	
+	def __unicode__(self):
+		return u'%d-%d %d:%d-%d:%d "%s"' % (self.date.month, self.date.day, self.begin.hour, self.begin.minute, self.end.hour, self.end.minute, self.summary)
+
 class ScheduleAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('belongs_to', 'one_of', '__unicode__')
+	list_links = ('__unicode__',)
 
 def fetch_assignments(student_id):
 	"""
@@ -77,6 +105,8 @@ def fetch_assignments(student_id):
 	taking_courses = []
 	try:
 		scholar_db = Sybase.connect(settings.SCHOLARDB_HOST, settings.SCHOLARDB_USER, settings.SCHOLARDB_PASSWORD, settings.SCHOLARDB_NAME)
+	except NameError:
+		raise DatabaseError('Sybase module is not installed!')
 	except:
 		raise DatabaseError('Cannot access the scholar database!')
 	cursor = scholar_db.cursor()
@@ -93,6 +123,8 @@ def fetch_assignments(student_id):
 	assignments = []
 	try:
 		moodle_db = MySQLdb.connect(host=settings.MOODLEDB_HOST, user=settings.MOODLEDB_USER, passwd=settings.MOODLEDB_PASSWORD, db=settings.MOODLEDB_NAME, use_unicode=True, charset='utf8')
+	except NameError:
+		raise DatabaseError('MySQLdb module is not installed!')
 	except:
 		raise DatabaseError('Cannot access the moodle database!')
 	cursor = moodle_db.cursor()
