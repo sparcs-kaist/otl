6c71de09b8b82dc542ae901e684b622252f68ffa
daybreaker <devnull@localhost>
2009-09-01 23:34:53 +0900

    refs #50: 흑백 한글 버전 PDF 출력 기능 완성

--- otl/utils/__init__.py
+++ otl/utils/__init__.py
@@ -44,7 +44,7 @@ def respond_as_json(request, obj):
     type = 'application/json' if request.is_ajax() else 'text/plain'
     return HttpResponse(output, mimetype=type)
 
-def respond_as_attachment(request, file_path, original_filename):
+def respond_as_attachment(request, file_path, original_filename, no_attach=False):
     fp = open(file_path, 'rb')
     response = HttpResponse(fp.read())
     fp.close()
@@ -56,18 +56,19 @@ def respond_as_attachment(request, file_path, original_filename):
     if encoding is not None:
         response['Content-Encoding'] = encoding
 
-    # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
-    if u'WebKit' in request.META['HTTP_USER_AGENT']:
-        # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
-        filename_header = 'filename=%s' % original_filename.encode('utf-8')
-    elif u'MSIE' in request.META['HTTP_USER_AGENT']:
-        # IE does not support internationalized filename at all.
-        # It can only recognize internationalized URL, so we do the trick via routing rules.
-        filename_header = ''
-    else:
-        # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
-        filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote(original_filename.encode('utf-8'))
-    response['Content-Disposition'] = 'attachment; ' + filename_header
+    if not no_attach:
+        # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
+        if u'WebKit' in request.META['HTTP_USER_AGENT']:
+            # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
+            filename_header = 'filename=%s' % original_filename.encode('utf-8')
+        elif u'MSIE' in request.META['HTTP_USER_AGENT']:
+            # IE does not support internationalized filename at all.
+            # It can only recognize internationalized URL, so we do the trick via routing rules.
+            filename_header = ''
+        else:
+            # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
+            filename_header = 'filename*=UTF-8\'\'%s' % urllib.quote(original_filename.encode('utf-8'))
+        response['Content-Disposition'] = 'attachment; ' + filename_header
     return response
 
 def date_range(start, end):
